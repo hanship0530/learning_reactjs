fa58cfbdb248214b343f75929585ffe59a542ec4
Hangeul Bae <hanship0531@chunjae.co.kr>
2021-03-02 17:18:58 +0900

    Section3: 43 Functions Components Naming

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -25,12 +25,33 @@ import Person from './Person/Person'
 // }
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 26}
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({persons: [
+      { name: 'Maximilian', age: 27},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 26}
+    ]})
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
         <p>This is really working</p>
-        <Person/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     )
     // return React.createElement('div', {className: 'App'}, React.createElement('h1',null,'Does this work now?'));
